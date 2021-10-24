c61d2085a75b9821a3ba6cf302398713f3767900
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-10-24 16:54:14 +0300

    test initialization

--- user_test.py
+++ user_test.py
@@ -10,14 +10,14 @@ class test_user (unittest.TestCase):
     def tearDown(self):
         User.list_user = []
 
-    # def test_init(self):
-    #     self.assertEqual(self.new_user.first_name, "Timothy")
-    #     self.assertEqual(self.new_user.last_name, "Mugendi")
-    #     self.assertEqual(self.new_user.password, "987900")
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "Timothy")
+        self.assertEqual(self.new_user.last_name, "Mugendi")
+        self.assertEqual(self.new_user.password, "987900")
 
     def test_save(self):
         self.new_user.save_details()
-        self.assertEqual(len(User.list_user),1)
+        self.assertEqual(len(User.list_user), 1)
 
     def test_delete(self):
         self.new_user.save_details()
